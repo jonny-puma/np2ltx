38b40b14c0b4f1320fbfc0bd44a17ee6622ba88f
jonny-puma <51737527+jonny-puma@users.noreply.github.com>
2020-10-01 13:48:24 +0200

    Update latex_parser.py

--- np2ltx/latex_parser.py
+++ np2ltx/latex_parser.py
@@ -8,7 +8,19 @@ def matrix2string(M, name):
     # Parse numpy matrix into latex
     for i in range(n):
         for j in range(m):
-            ltx += f"{M[i,j]:1.4}" + (" & " if j < m-1 else " \\\\ \n ")
+            entry = M[i,j]
+            entry_type = type(entry)
+            
+            # format according to type
+            if entry_type == float:
+                ltx += f"{entry:1.4}"
+            elif entry_type = bool
+                ltx += str(entry*1)
+            else:
+                ltx += str(entry)
+                
+            # next column or next row
+            ltx += (" & " if j < m-1 else " \\\\ \n ")
     
     return f"""
             {name} = 
