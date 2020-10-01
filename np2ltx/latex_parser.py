9fcd5883aa0d08b1e346218f940c93f10cfaa7a8
jonny-puma <51737527+jonny-puma@users.noreply.github.com>
2020-10-01 12:23:02 +0200

    Update latex_parser.py

--- np2ltx/latex_parser.py
+++ np2ltx/latex_parser.py
@@ -8,22 +8,19 @@ def matrix2string(M, name):
     # Parse numpy matrix into latex
     for i in range(n):
         for j in range(m):
-            ltx += f"{M[i,j]:1.4}" + (" & " if j < m-1 else " \\\\ \n ")
-
-    return ltx
+            ltx += f"{M[i,j]:1.4}" + (" & " if j < m-1 else " \\ \n ")
+    
+    return fr"""
+            {name} = 
+            \begin{{bmatrix}}
+            {ltx}
+            \end{{bmatrix}}
+            """
 
 def printMatrix(M, name):
     ltx = matrix2string(M, name)
 
     # Display results as latex with some padding
-    display(Math(
-        fr"""
-        \\~\\
-        {name} = 
-        \begin{{bmatrix}}
-        {ltx}
-        \end{{bmatrix}}
-        \\~\\
-        """))
+    display(Math("\\~\\" + ltx + "\\~\\"))
     
     
